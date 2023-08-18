ab12d43040702ffff6920466f139ee6e52483f55
AlgorithmEncoder <algorithmencoder@gmail.com>
2023-08-18 14:28:59 +0200

    Implementación para que el heroe no se salga del canvas

--- js/game.js
+++ js/game.js
@@ -65,16 +65,16 @@ var reset = function () {
 // Update game objects
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+		if ((hero.y - hero.speed * modifier) > 0) hero.y -= hero.speed * modifier;
 	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+	else if (40 in keysDown) { // Player holding down
+		if ((hero.y + hero.speed * modifier) < 450) hero.y += hero.speed * modifier;
 	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+	else if (37 in keysDown) { // Player holding left
+		if ((hero.x - hero.speed * modifier) > 0) hero.x -= hero.speed * modifier;
 	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+	else if (39 in keysDown) { // Player holding right
+		if ((hero.x + hero.speed * modifier) < 480) hero.x += hero.speed * modifier;
 	}
 
 	// Are they touching?
